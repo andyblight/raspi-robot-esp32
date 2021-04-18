e46fc1f00fadcd2e54639432c11a646c81e7ce43
Andy Blight <andy.blight@gmail.com>
2021-04-18 18:49:26 +0100

    Added diagnostics files

---
+++ rover/raspi_rover/diagnostics.c
@@ -0,0 +1,38 @@
+#include "diagnostics.h"
+
+#include <rclc/rclc.h>
+
+#include "diagnostic_msgs/msg/diagnostic_array.h"
+#include "diagnostic_msgs/msg/diagnostic_status.h"
+#include "diagnostic_msgs/msg/key_value.h"
+#include "rosidl_runtime_c/string_functions.h"
+
+#define NUM_STATUS (2)
+
+static void fill_robot_status(diagnostic_msgs__msg__DiagnosticStatus *status) {
+  diagnostic_msgs__msg__DiagnosticStatus__init(&status);
+  // Serial number or similar.
+  rosidl_runtime_c__String__assign(&status.hardware_id, "RPR1");
+  // `level` is one of:
+  // diagnostic_msgs__msg__DiagnosticStatus__OK
+  // diagnostic_msgs__msg__DiagnosticStatus__WARN
+  // diagnostic_msgs__msg__DiagnosticStatus__ERROR
+  // diagnostic_msgs__msg__DiagnosticStatus__STALE
+  status.level = diagnostic_msgs__msg__DiagnosticStatus__OK;
+  // A descriptive message.
+  rosidl_runtime_c__String__assign(&status.message, "AOK");
+  // Robot name.
+  rosidl_runtime_c__String__assign(&status.name, "RasPiRover");
+  // TODO Add key value pairs here.
+}
+
+static void add_wifi_status(diagnostic_msgs__msg__DiagnosticStatus *status) {
+  RCLC_UNUSED(status);
+}
+
+void diagnostics_populate(diagnostic_msgs__msg__DiagnosticArray *msg) {
+  diagnostic_msgs__msg__DiagnosticStatus robot_status;
+  diagnostic_msgs__msg__DiagnosticStatus wifi_status;
+  fill_robot_status(&robot_status);
+  fill_wifi_status(&wifi_status);
+}
---
+++ rover/raspi_rover/diagnostics.h
@@ -0,0 +1,8 @@
+#ifndef DIAGNOSTICS_H
+#define DIAGNOSTICS_H
+
+#include "diagnostic_msgs/msg/diagnostic_array.h"
+
+void diagnostics_populate(diagnostic_msgs__msg__DiagnosticArray *msg);
+
+#endif // DIAGNOSTICS_H
